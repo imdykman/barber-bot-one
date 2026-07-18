569618e153d5d5e55748a09b3e9e9b43ff607f8b
imdykman <imdykman@gmail.com>
2026-07-18 11:38:36 +0500

    feat: добавить управление мастерами и услугами в админку (через роутинг)

--- handlers/admin/index.js
+++ handlers/admin/index.js
@@ -7,6 +7,8 @@ async function showAdminMenu(ctx, userId) {
     [Keyboard.button.callback('📋 Все записи', 'admin_all_bookings')],
     [Keyboard.button.callback('📊 Статистика', 'admin_stats')],
     [Keyboard.button.callback('📥 Экспорт в CSV', 'admin_export')],
+    [Keyboard.button.callback('👨‍💼 Мастера', 'admin_masters')],
+    [Keyboard.button.callback('💈 Услуги', 'admin_services')],
     [Keyboard.button.callback('🏠 Выйти из админки', 'start')],
   ]);
 
