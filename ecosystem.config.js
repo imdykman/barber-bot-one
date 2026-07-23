900aa5de172daf23388ea2c238c744c7c2990bf5
imdykman <imdykman@gmail.com>
2026-07-23 21:31:46 +0500

    fix: set timezone to Asia/Yekaterinburg for correct time handling

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,20 @@
 module.exports = {
   apps: [
     {
-      name: 'barber-bot',
+      name: 'barber-one',
       script: 'bot.js',
       env: {
+        TZ: 'Asia/Yekaterinburg',
         NODE_EXTRA_CA_CERTS: './russian_trusted_root_ca.cer',
         NODE_ENV: 'production',
       },
     },
     {
-      name: 'barber-web',
+      name: 'barber-one-web',
       script: 'server.js',
       env: {
+        TZ: 'Asia/Yekaterinburg',
+        PORT: 3001,
         NODE_ENV: 'production',
       },
     },
